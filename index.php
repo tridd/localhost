269ff557c59e0bcbb0cadf301217f3cf3224c7f7
tridd <kalindim777@gmail.com>
2017-02-14 00:11:01 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//echo 123;
+//echo '2141244124dfgsgdsgsdg';
+//echo PHP_EOL;
+//https://github.com/tridd/localhost.git
+if (true);
+else;
+//
+//while (true) {
+//    // exit();
+//    var_dump('1245135');die();
+//}
+for($i=0, $j = 1; $i < 20 || $j < 10; $i++)
+{
+    $str2 = sprintf('I = %s; J = %s; %s', $i, $j, '</br>');
+    echo $str2;
+    $j++;
+}
